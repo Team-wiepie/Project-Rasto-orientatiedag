0eafacd81c8e61c9e46fac7860186c4defaa3584
pieter21 <pietvanduijvenbode@hotmail.>
2013-09-05 14:00:05 +0200

    yeah ouhm lg ing scherm enzo

---
+++ student_aanmelden/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link type="text/css" rel="stylesheet" href="global.css.css" />
+        <title>student aanmelden!</title>
+    </head>
+    <body>
+        <div id="opleiding_main">
+        <h2>opleiding toevoegen</h2>
+        <input type="text" placeholder="opleiding" id="opleiding">
+        <br>
+        <input type="text" placeholder="locatie" id="locatie">
+        <br>
+        <input type="text" placeholder="opleidings code" id="opldeiding_code">
+        <br>
+        <input type="text" placeholder="categorie" id="categorie">
+        <br>
+        <input type="button" value="voeg toe!">
+        </div>
+        <?php
+        
+        ?>
+    </body>
+</html>
