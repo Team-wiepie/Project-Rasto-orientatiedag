016fa6e78550e9763d33ecdfde201be980c8f55b
longlifebattery <koen_zandvliet@hotmail.com>
2013-09-19 13:37:56 +0200

    Added new table "email" for saving the emails

--- Database/orientatiedag.sql
+++ Database/orientatiedag.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Genereertijd: 06 sep 2013 om 12:04
+-- Genereertijd: 19 sep 2013 om 13:24
 -- Serverversie: 5.5.32
 -- PHP-versie: 5.4.16
 
@@ -56,6 +56,18 @@ CREATE TABLE IF NOT EXISTS `decanen` (
   `email` varchar(40) DEFAULT NULL,
   `telefoon` int(15) DEFAULT NULL,
   PRIMARY KEY (`decaan_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `email`
+--
+
+CREATE TABLE IF NOT EXISTS `email` (
+  `email_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` longtext,
+  PRIMARY KEY (`email_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
