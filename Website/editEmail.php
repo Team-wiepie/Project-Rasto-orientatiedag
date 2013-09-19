85f612c4a9be63c2507a9ae8ac0f9a353e0c59e9
WimFris <wimfris.tk@gmail.com>
2013-09-19 16:33:18 +0200

    Swaggercmit

--- Website/editEmail.php
+++ Website/editEmail.php
@@ -20,7 +20,7 @@
 		<div id="resultaat">
 			<?php if(isset($_POST['submit'])){echo "<b>Het E-Mail bericht is bijgewerkt!</b><br>";} ?>
 			Voorbeeld van de E-Mail:<br>
-			<?php echo $text['email']; ?>
+			<?php if(isset($_POST['submit'])){echo $_POST['emailText'];}else{echo $text['email'];} ?>
 		</div>
 		
 		<form action="editEmail.php" method="POST">
